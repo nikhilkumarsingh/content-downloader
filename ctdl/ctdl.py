640d4f8b38bc6fda5cee7d291c8dd224fe1a07e7
nikhilkumarsingh <nikhilksingh97@gmail.com>
2017-05-23 14:31:30 +0530

    little reformatting

--- ctdl/ctdl.py
+++ ctdl/ctdl.py
@@ -4,10 +4,11 @@ import requests
 from requests.packages.urllib3.util.retry import Retry
 from requests.adapters import HTTPAdapter
 from bs4 import BeautifulSoup
-from .downloader import download_series, download_parallel
-from .utils import FILE_EXTENSIONS, THREAT_EXTENSIONS
+from downloader import download_series, download_parallel
+from utils import FILE_EXTENSIONS, THREAT_EXTENSIONS
 
 search_url = "https://www.google.com/search"
+
 s = requests.Session()
 # Max retries and back-off strategy so all requests to http:// sleep before retrying
 retries = Retry(total=5,
@@ -15,14 +16,16 @@ retries = Retry(total=5,
                 status_forcelist=[ 500, 502, 503, 504 ])
 s.mount('http://', HTTPAdapter(max_retries=retries))
 
+
 def scrape(html):
-	soup = BeautifulSoup(html, 'html5lib')
+	soup = BeautifulSoup(html, 'lxml')
 	results = soup.findAll('h3', {'class': 'r'})
 	links = []
 	for result in results:
 		link = result.a['href'][7:].split('&')[0]
 		links.append(link)
 	return links
+
 
 def get_links(limit, params, headers):
     """
@@ -37,12 +40,14 @@ def get_links(limit, params, headers):
         links.extend(page_links)
     return links[:limit]
 
+
 def validate_links(links):
     valid_links = []
     for link in links:
         if link[:7] in "http://" or link[:8] in "https://":
             valid_links.append(link)
     return valid_links
+
 
 def search(query, file_type = 'pdf', limit = 10):
     gquery = "filetype:{0} {1}".format(file_type, query)
@@ -66,10 +71,13 @@ def check_threats(**args):
             for el in val:
                 if args['file_type'] == el:
                     is_high_threat = True
+                    break
         else:
             if args['file_type'] == val:
                 is_high_threat = True
+                break
     return is_high_threat
+
 
 def validate_args(**args):
     if not args['query']:
@@ -99,6 +107,7 @@ def show_filetypes(extensions):
             val = ", ".join(str(x) for x in item[1])
         print("{0:4}: {1}".format(val, item[0]))
 
+
 def main():
     parser = argparse.ArgumentParser(description = "Content Downloader",
     								 epilog="Now download files on any topic in bulk!")
@@ -107,7 +116,7 @@ def main():
     parser.add_argument("query", type = str, default = None, nargs = '?',
     					help = "Specify the query.")
 
-    parser.add_argument("-g", "--file_type", type = str, default = 'pdf',
+    parser.add_argument("-f", "--file_type", type = str, default = 'pdf',
                         help = "Specify the extension of files to download.")
      
     parser.add_argument("-l", "--limit", type = int, default = 10,
@@ -150,7 +159,7 @@ def main():
                     res = None
             return res
         prompt(
-            message = "WARNING: Downloading this file type may expose you to a heightened security risk.\nPress 'y' to proceed or 'n' to exit...\n",
+            message = "WARNING: Downloading this file type may expose you to a heightened security risk.\nPress 'y' to proceed or 'n' to exit",
             errormessage= "Error: Invalid option provided.",
             isvalid = lambda x:True if x is 'y' else None,
             isexit = lambda x:True if x is 'n' else None
