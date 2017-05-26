7527c369febeffcd6e0692cf4c38e33aaa54b95a
Luke Schoen <ltfschoen@gmail.com>
2017-05-26 16:07:47 +1000

    fix: Fix to allow use of `ctdl` instead of `python ctdl/ctdl.py`. Move Flask to examples.

--- examples/server.py
+++ examples/server.py
@@ -2,8 +2,21 @@
 from __future__ import print_function
 import sys
 from flask import Flask, jsonify, request, make_response, abort
+
+import site
+
+def get_main_path():
+    test_path = sys.path[0] # sys.path[0] is current path in 'examples' subdirectory
+    split_on_char = "/"
+    return split_on_char.join(test_path.split(split_on_char)[:-1])
+main_path = get_main_path()
+site.addsitedir(main_path+'/examples')
+site.addsitedir(main_path+'/ctdl')
+print ("Imported subfolder: %s" % (main_path+'/examples') )
+print ("Imported subfolder: %s" % (main_path+'/ctdl') )
+
 import ctdl
-from ctdl import main
+from ctdl.ctdl import main
 
 """
 RESTful web service endpoints using Flask.
@@ -23,7 +36,7 @@ def get_query():
     print("Query Params: ", query_params, file=sys.stderr)
 
     def process_ctdl(query_params):
-        ctdl.main(query_params)
+        main(query_params)
     process_ctdl(query_params)
 
     return jsonify({'success':True}), 200, {'ContentType':'application/json'}
