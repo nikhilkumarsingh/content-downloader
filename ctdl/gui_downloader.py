ed89647db5f7a51581087e2a9208a3444f2115e0
Akansh Jain <akansh97531@gmail.com>
2017-06-06 11:04:34 +0530

    Update gui_downloader.py

--- ctdl/gui_downloader.py
+++ ctdl/gui_downloader.py
@@ -29,12 +29,15 @@ s = requests.Session()
 # Max retries and back-off strategy so all requests to http:// sleep before retrying
 retries = Retry(total = 5, 
                 backoff_factor = 0.1, 
-                status_forcelist = [500,  502,  503,  504] )
-s.mount('http://', HTTPAdapter(max_retries = retries) )
+                status_forcelist = [500,  502,  503,  504])
+s.mount('http://', HTTPAdapter(max_retries = retries))
 
 
 def download(urls, directory, idx, min_file_size = 0, max_file_size = -1,  
              no_redirects = False, pos = 0, mode = 's'):
+    """
+    download function for serial download
+    """
     global main_it
     global exit_flag
     global total_chunks
@@ -89,6 +92,9 @@ def download(urls, directory, idx, min_file_size = 0, max_file_size = -1,
 
 def download_parallel(url, directory, idx, min_file_size = 0, max_file_size = -1,  
              no_redirects = False, pos = 0, mode = 's'):
+    """
+    download function to download parallely
+    """
     global main_it
     global exit_flag
     global total_chunks
@@ -103,12 +109,12 @@ def download_parallel(url, directory, idx, min_file_size = 0, max_file_size = -1
 
 
 
-    resp = s.get(url, stream = True, allow_redirects = is_redirects )
+    resp = s.get(url, stream = True, allow_redirects = is_redirects)
     if not resp.status_code == 200:
         # ignore this file since server returns invalid response
         return
     try:
-        total_size = int(resp.headers['content-length'] )
+        total_size = int(resp.headers['content-length'])
     except KeyError:
         total_size = len(resp.content)
 
@@ -140,6 +146,9 @@ def download_parallel(url, directory, idx, min_file_size = 0, max_file_size = -1
 
 
 class myThread (threading.Thread):
+    """
+    custom thread to run download thread
+    """
     def __init__(self, url, directory, idx, min_file_size, max_file_size, no_redirects):
         threading.Thread.__init__(self)
         self.idx=idx
@@ -152,20 +161,24 @@ class myThread (threading.Thread):
 
 
     def run(self):
-        # print ( "Starting")
+        """
+        called when thread is started
+        """
         if parallel:
             # here url is single url
             download_parallel(self.url, self.directory, self.idx 
-                            , self.min_file_size, self.max_file_size, self.no_redirects )
+                            , self.min_file_size, self.max_file_size, self.no_redirects)
         else:
             # here url is whole list of url
             download(self.url,  self.directory, self.idx  
-                           , self.min_file_size, self.max_file_size, self.no_redirects )
+                           , self.min_file_size, self.max_file_size, self.no_redirects)
 
 
 
-class progress_class( ):
-
+class progress_class():
+    """
+    custom class for profress bar
+    """
     def __init__(self, frame, url, directory,  min_file_size, max_file_size, no_redirects):
         global i_max
         global file_name
@@ -200,10 +213,10 @@ class progress_class( ):
 
             # initialize progressbar
             self.progress.append( ttk.Progressbar(frame, 
-                                     orient="horizontal", length=300, mode="determinate") )
+                                     orient="horizontal", length=300, mode="determinate"))
             self.progress[self.i].pack()
-            self.str.append( StringVar() )
-            self.label.append( Label(frame, textvariable=self.str[self.i], width=40) )
+            self.str.append( StringVar())
+            self.label.append( Label(frame, textvariable=self.str[self.i], width=40))
             self.label[self.i].pack()
             self.progress[self.i]["value"] = 0
             self.bytes.append(0)
@@ -215,17 +228,17 @@ class progress_class( ):
         self.start()
 
 
-    def start( self):
+    def start(self):
 
 
         for self.i in range(0, self.length) :
             if parallel:
                 self.thread.append( myThread(self.url[ self.i ], self.directory, self.i 
-                                  , self.min_file_size, self.max_file_size, self.no_redirects) )
+                                  , self.min_file_size, self.max_file_size, self.no_redirects))
             else:
                 # if not parallel whole url list is passed
                 self.thread.append( myThread(self.url, self.directory, self.i , self.min_file_size 
-                                  ,  self.max_file_size,  self.no_redirects) )
+                                  ,  self.max_file_size,  self.no_redirects))
             self.progress[self.i]["value"] = 0
             self.bytes[self.i] = 0
             self.thread[self.i].start()
@@ -235,7 +248,9 @@ class progress_class( ):
 
 
     def read_bytes(self):
-        ''' reading bytes; update progress bar after 1 ms'''
+        """
+        reading bytes; update progress bar after 1 ms
+        """
         global exit_flag
 
         for self.i in range(0, self.length) :
@@ -243,8 +258,8 @@ class progress_class( ):
             self.maxbytes[self.i] = total_chunks[self.i]
             self.progress[self.i]["maximum"] = total_chunks[self.i]
             self.progress[self.i]["value"] = self.bytes[self.i]
-            self.str[self.i].set( file_name[self.i]+ "       " + str( self.bytes[self.i]) 
-                                  + "KB / " + str( int( self.maxbytes[self.i] + 1)  ) + " KB" )
+            self.str[self.i].set(file_name[self.i]+ "       " + str( self.bytes[self.i]) 
+                                  + "KB / " + str( int( self.maxbytes[self.i] + 1)  ) + " KB")
 
 
         if exit_flag == self.length:
@@ -257,6 +272,9 @@ class progress_class( ):
 
 
 def download_parallel_gui(root, urls,  directory,  min_file_size,  max_file_size,  no_redirects):
+    """
+    called when paralled downloading is true
+    """
     global main_iter
     global parallel
 
@@ -271,7 +289,9 @@ def download_parallel_gui(root, urls,  directory,  min_file_size,  max_file_size
 
 
 def download_series_gui(frame, urls, directory, min_file_size, max_file_size, no_redirects):
-
+    """
+    called when user wants serial downloading
+    """
     # create directory to save files
     if not os.path.exists(directory):
         os.makedirs(directory)
