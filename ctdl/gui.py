d784ced30372864e4f4e545589e33ab1e8aebd8f
Akansh Jain <akansh97531@gmail.com>
2017-06-06 10:58:36 +0530

    Update gui.py

--- ctdl/gui.py
+++ ctdl/gui.py
@@ -32,9 +32,9 @@ from tqdm import tqdm,  trange
 
 # icon and title
 root = Tk()
-root.wm_title("Content Downloader")
+root.wm_title("ctdl")
 try:
-    img = PhotoImage( file  =  "icon.png" )
+    img = PhotoImage(file  =  "icon.png")
     root.tk.call('wm',  'iconphoto',  root._w,  img)
 except:
     img = Image("photo", file="icon.gif")
@@ -45,6 +45,9 @@ links = []
 
 
 def search_function(root1, q, f, l):
+    """
+    function to get links
+    """
     global links
     links = search(q, f, l)
     root1.destroy()
@@ -54,26 +57,30 @@ def search_function(root1, q, f, l):
 
 
 def task(ft):
-    ft.pack(expand = True,  fill = BOTH,  side = TOP )
-    pb_hD = ttk.Progressbar(ft, orient = 'horizontal', mode = 'indeterminate' )
-    pb_hD.pack(expand = True, fill = BOTH, side = TOP )
+    """
+    to create loading progress bar
+    """
+    ft.pack(expand = True,  fill = BOTH,  side = TOP)
+    pb_hD = ttk.Progressbar(ft, orient = 'horizontal', mode = 'indeterminate')
+    pb_hD.pack(expand = True, fill = BOTH, side = TOP)
     pb_hD.start(50)
     ft.mainloop()
 
 
 
-def download_content_gui( **args ):
+def download_content_gui(**args):
+    """
+    function to fetch links and download them
+    """
     global row
-    """
-    main function to fetch links and download them
-    """
+
     if not args ['directory']:
         args ['directory'] = args ['query'].replace(' ', '-')
 
 
     root1 = Frame(root)
     t1 = threading.Thread(target = search_function,  args =(root1,
-                          args ['query'],  args ['file_type'],  args ['limit'],  ) )
+                          args ['query'],  args ['file_type'],  args ['limit'],))
     t1.start()
     task(root1)  # This will block while the mainloop runs
     t1.join()
@@ -84,10 +91,10 @@ def download_content_gui( **args ):
     row.pack()
     if args['parallel']:
         download_parallel_gui(row, links,  args ['directory'], args ['min_file_size'], 
-                                 args ['max_file_size'],  args ['no_redirects'] )
+                                 args ['max_file_size'],  args ['no_redirects'])
     else:
         download_series_gui(row, links, args ['directory'], args ['min_file_size'],
-                                 args ['max_file_size'], args ['no_redirects'] )
+                                 args ['max_file_size'], args ['no_redirects'])
 
 
 
@@ -103,52 +110,55 @@ args = { 'parallel' : False, 'file_type' : 'pdf', 'threats' : False,
 
 
 class makeform:
+    """
+    to makre the main form of gui
+    """
     global args
     def __init__(self, root):
 
 
         # label search query
-        self.row0 = Frame(root )
+        self.row0 = Frame(root)
         self.lab0 = Label(self.row0, width = 25, text = fields [0], anchor = 'w')
         self.entry_query = Entry(self.row0)
         self.entry_query.insert(0, 'python')
         self.entry_query.bind('<FocusIn>',  self.on_entry_click)
         self.entry_query.bind('<FocusOut>',  lambda event,  
-                               a = "query" : self.on_focusout(event, a ))
+                               a = "query" : self.on_focusout(event, a))
 
         self.entry_query.config(fg = 'grey')
-        self.row0.pack(side = TOP,  fill = X,  padx = 5,  pady = 5 )
-        self.lab0.pack( side = LEFT )
-        self.entry_query.pack( side = RIGHT,  expand = YES,  fill = X )
+        self.row0.pack(side = TOP,  fill = X,  padx = 5,  pady = 5)
+        self.lab0.pack(side = LEFT)
+        self.entry_query.pack( side = RIGHT,  expand = YES,  fill = X)
 
 
 
         # label min_file_size
-        self.row1  =  Frame( root )
-        self.lab1  =  Label( self.row1,  width = 25,  text = fields [1],  anchor = 'w' )
-        self.entry_min  =  Entry( self.row1 )
-        self.entry_min.insert( 0,  '0' )
-        self.entry_min.bind( '<FocusIn>',  self.on_entry_click )
-        self.entry_min.bind( '<FocusOut>',  lambda event,  
-                              a = "min_file_size": self.on_focusout( event, a )  )
+        self.row1  =  Frame(root)
+        self.lab1  =  Label(self.row1,  width = 25,  text = fields [1],  anchor = 'w')
+        self.entry_min  =  Entry(self.row1)
+        self.entry_min.insert(0,  '0')
+        self.entry_min.bind('<FocusIn>',  self.on_entry_click)
+        self.entry_min.bind('<FocusOut>',  lambda event,  
+                              a = "min_file_size": self.on_focusout( event, a))
 
-        self.entry_min.config( fg  =  'grey' )
-        self.row1.pack( side = TOP,  fill = X,  padx = 5,  pady = 5 )
-        self.lab1.pack( side = LEFT )
-        self.entry_min.pack( side = RIGHT,  expand = YES,  fill = X )
+        self.entry_min.config(fg = 'grey')
+        self.row1.pack(side = TOP, fill = X,  padx = 5,  pady = 5)
+        self.lab1.pack(side = LEFT)
+        self.entry_min.pack( side = RIGHT,  expand = YES,  fill = X)
 
 
 
         # label max_file_size
-        self.row2  =  Frame( root )
-        self.lab2  =  Label( self.row2,  width = 25,  text = fields [2],  anchor = 'w' )
-        self.entry_max  =  Entry( self.row2 )
+        self.row2  =  Frame(root)
+        self.lab2  =  Label(self.row2,  width = 25,  text = fields [2],  anchor = 'w')
+        self.entry_max  =  Entry(self.row2)
         self.entry_max.insert(0, '-1')
         self.entry_max.bind('<FocusIn>',  self.on_entry_click)
         self.entry_max.bind('<FocusOut>', lambda event,  
                              a = "max_file_size": self.on_focusout(event, a))
 
-        self.entry_max.config( fg  =  'grey')
+        self.entry_max.config(fg  =  'grey')
         self.row2.pack(side = TOP, fill = X, padx = 5, pady = 5)
         self.lab2.pack(side = LEFT )
         self.entry_max.pack(side = RIGHT, expand = YES, fill = X)
@@ -172,7 +182,7 @@ class makeform:
 
 
         # label download limit
-        self.row4 = Frame(root )
+        self.row4 = Frame(root)
         self.lab4 = Label(self.row4, width = 25, text = fields[4], anchor = 'w')
         self.entry_limit = Entry(self.row4)
         self.entry_limit.insert(0, '10')
@@ -181,7 +191,7 @@ class makeform:
                             a = "limit" : self.on_focusout(event, a))
         self.entry_limit.config(fg = 'grey')
         self.row4.pack(side = TOP, fill = X, padx = 5, pady = 5)  
-        self.lab4.pack(side = LEFT )
+        self.lab4.pack(side = LEFT)
         self.entry_limit.pack(side = RIGHT, expand = YES, fill = X)
 
 
@@ -236,7 +246,7 @@ class makeform:
 
         # download button
         self.row7 = Frame(root)
-        self.search_button = Button(self.row7, width = 15, text = "Download", anchor = 'w' )
+        self.search_button = Button(self.row7, width = 15, text = "Download", anchor = 'w')
         self.search_button.bind('<Button-1>', self.click_download)
         self.row7.pack(side = TOP,  fill = X, padx = 5, pady = 5)
         self.search_button.pack(side = LEFT)
@@ -254,27 +264,32 @@ class makeform:
         args ['file_type'] = self.optionmenu.get()
         args ['no_redirects'] = self.t.get()
         args ['query'] = self.entry_query.get()
-        args ['min_file_size'] = int( self.entry_min.get() )
-        args ['max_file_size'] = int( self.entry_max.get() )
-        args ['limit'] = int( self.entry_limit.get() )
+        args ['min_file_size'] = int( self.entry_min.get())
+        args ['max_file_size'] = int( self.entry_max.get())
+        args ['limit'] = int( self.entry_limit.get())
         self.check_threat()
         download_content_gui( **args )
 
 
 
     def on_entry_click(self, event):
-        """function that gets called whenever entry is clicked"""
+        """
+        function that gets called whenever entry is clicked
+        """
         if event.widget.config('fg') [4] == 'grey':
            event.widget.delete(0, "end" ) # delete all the text in the entry
-           event.widget.insert(0, '' ) #Insert blank for user input
+           event.widget.insert(0, '') #Insert blank for user input
            event.widget.config(fg = 'black')
 
 
 
     def on_focusout(self, event, a):
-        if event.widget.get(  )  ==  '':
-            event.widget.insert( 0, default_text [a])
-            event.widget.config( fg = 'grey')
+        """
+        function that gets called whenever anywhere except entry is clicked
+        """
+        if event.widget.get()  ==  '':
+            event.widget.insert(0, default_text [a])
+            event.widget.config(fg = 'grey')
 
 
 
@@ -284,7 +299,7 @@ class makeform:
         """
         is_high_threat = False
         for val in THREAT_EXTENSIONS.values():
-            if type( val ) == list:
+            if type(val) == list:
                 for el in val:
                     if self.optionmenu.get() == el:
                         is_high_threat = True
@@ -295,32 +310,32 @@ class makeform:
                     break
 
         if is_high_threat == True:
-            is_high_threat = not askokcancel( 'FILE TYPE',  'WARNING: Downloading this \
+            is_high_threat = not askokcancel('FILE TYPE',  'WARNING: Downloading this \
                                             file type may expose you to a heightened security risk.\nPress\
-                                             "OK" to proceed or "CANCEL" to exit' )
+                                             "OK" to proceed or "CANCEL" to exit')
         return not is_high_threat
 
     def ask_dir(self):
-        # dialogue box for choosing directory
-        args ['directory'] = askdirectory( **self.dir_opt ) 
-        self.dir_text.set( args ['directory'] )
+        """
+        dialogue box for choosing directory
+        """
+        args ['directory'] = askdirectory(**self.dir_opt) 
+        self.dir_text.set(args ['directory'])
 
-    def clear_fun( self, event ):
+    def clear_fun(self, event):
         global row
-        row.destroy(  )
+        row.destroy()
 
 
 
 
-def main(  ):
-
-
-    # app  =  SampleApp(  )
-
+def main():
+    """
+    main function
+    """
     s = ttk.Style(  )
     s.theme_use( 'clam' )
     ents  =  makeform( root )
-
 
     root.mainloop(  )
 
